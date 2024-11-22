0d8e9582af1efb72e8afacae3be2538bc63a0917
Dante Augusto <danteaugusto.10@gmail.com>
2024-11-22 12:43:10 -0300

    Initial commit

---
+++ src/test/java/com/example/FioriDiMaria/FioriDiMariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FioriDiMaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FioriDiMariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
